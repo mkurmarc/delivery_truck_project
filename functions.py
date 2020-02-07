df19318fee843f0f9513ede0e5f7fee0c9ddc2c6
Marc Rios <marc.rios1712@gmail.com>
2020-02-06 17:01:46 -0800

    added init, str dunder, calc time methods. calc time is rough

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 # Marc
+from time import time
+
 class HashTable:
 
 # initializes the object and creates the empty hash table with empty buckets
@@ -66,6 +68,27 @@ class CreateDict:
     def __str__(self):
         return str(self.dictionary)
 
+class Truck:
+
+    def __init__(self, s_time):
+        self.start_time = s_time
+        self.acc_time = s_time
+        self.truck_cargo = []
+
+    def __str__(self):
+        return (f"Truck's cargo includes {self.truck_cargo}\nTruck left from hub at {self.start_time}\
+                     \nTotal time is {self.acc_time}") # use total time to mark packages on board as either in transit, delivered, or at hub
+
+    # function to calculate time given distance and speed
+    def calculate_time(self, dist, speed):
+        print("Distance(km) :", dist);
+        print("Speed(km / hr) :", speed);
+        return speed * dist;
+
+# Distance  = Speed * Time
+# Time = Distance / Speed
+# Speed = Distance / Time
+
 
 
 ################################ Program Script Below ################################ transfer to main later
@@ -78,6 +101,8 @@ map_matrix = []
 
 # creates empty address dictionary that will be used to map an address to its corresponding index to the list in the map matrix
 address_dictionary = CreateDict()
+
+truck_1 = Truck('08:00:00')
 
 # this reads file line by line and creates a dictionary of packages' info. Then inputs the dictionary into the hash table with set_val method.
 #Also, with the read in data, the add_to_dict method adds to the empty dictionary.
@@ -103,3 +128,8 @@ with open("wgups_distance_table.txt") as f:
 # print(hash_table)
 # print(map_matrix)
 # print(address_dictionary)
+
+print(truck_1)
+
+# Calling function cal_time() using rate of 18 mph
+print("The calculated Time(hr) :", truck_1.calculate_time(100, 18));
